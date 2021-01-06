ca78d9d92bb76b74cff45f84ea7da98cfada4002
Isaque Ribeiro <isaque.ribeiro.bernardes@gmail.com>
2021-01-06 00:29:43 -0300

    init project

---
+++ src/test/java/br/com/isaquebrb/iftm/springbatchdemo/SpringBatchDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.isaquebrb.iftm.springbatchdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBatchDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
